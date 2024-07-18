a7313ba6ff2646ea4c6714d74f166863ee9dd5c1
ainamzani <ainamzani@gmail.com>
2024-07-19 03:31:23 +0800

    Update LoadingBar.js

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -6,7 +6,8 @@ class LoadingBar{
 		this.domElement.style.left = '0';
 		this.domElement.style.width = '100%';
 		this.domElement.style.height = '100%';
-		this.domElement.style.background = '#DDE255';
+		//this.domElement.style.background = '#DDE255';
+		//this.domElement.style.backgroundImage = 
 		this.domElement.style.opacity = '0.7';
 		this.domElement.style.display = 'flex';
 		this.domElement.style.alignItems = 'center';
